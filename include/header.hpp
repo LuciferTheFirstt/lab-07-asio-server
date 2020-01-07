f423f3b670d7e3f40d683e7a10292de50b3ccb09
LuciferTheFirstt <dimonator67@gmail.com>
2020-01-07 23:10:59 +0300

    server

--- include/header.hpp
+++ include/header.hpp
@@ -13,104 +13,148 @@ using namespace std;
 using namespace boost::asio;
 
 io_service service;
+boost::recursive_mutex mx;
+class Client;
+std::vector<std::shared_ptr<Client>> clients;
 
-struct talk_to_svr
-{
-	talk_to_svr(const std::string & username): sock_(service), started_(true), username_(username) {}
-	
-	void connect(ip::tcp::endpoint ep){
-		sock_.connect(ep);
-	}
-	
-	void loop(){
-		write("login " + username_ + "\n");
-		read_answer();
-		while (started_){
-			write_request();
-			read_answer();
-			boost::this_thread::sleep(boost::posix_time::millisec(rand() % 4000));
-		}
-	}
-	std::string username() const { return username_; }
-    
-    void write_request(){
-	    write("ping\n");
-    }
-    void read_answer(){
-    	already_read_ =	read(sock_, buffer(buff_),
-    	boost::bind(&talk_to_svr::read_complete,this,_1, _2));
-    	process_msg();
-    }
-    void process_msg(){
-    	std::string msg(buff_, already_read_);
-
-    	if ( msg.find("login ") == 0) on_login(msg);
-    	else if ( msg.find("ping ") == 0) on_ping(msg);
-    	else if ( msg.find("clients ") == 0) on_clients(msg);
-    	else std::cerr << "invalid msg " << msg << std::endl;
-    }
-
-    void on_login(const std::string& msg) { 
-        std::cout<<msg<<std::endl;
-        do_ask_clients();
-        std::cout<<std::endl;
-    }
-    
-    void on_ping(const std::string & msg){
-        std::cout<<msg<<std::endl;
-    	std::istringstream in(msg);
-    	std::string answer;
-    	in >> answer >> answer;
-    	if ( answer == "client_list_changed")
-    	    do_ask_clients();
-    }
-    void on_clients(const std::string & msg){
-    	std::string clients = msg.substr(8);
-    	std::cout << username_ << ", new client list:" << clients << std::endl;
-    }
-    void do_ask_clients(){
-    	write("ask_clients\n");
-    	read_answer();
-    }
-    void write(const std::string& msg) {
-        sock_.write_some(buffer(msg)); 
-    }
-    
-    size_t read_complete(const boost::system::error_code & err, size_t bytes){
-    	std::cout<<bytes<<std::endl;
-	    if (err) return 0;
-		bool found = std::find(buff_, buff_ + bytes, '\n') < buff_ + bytes;
-		return found ? 0 : 1;
-    }
-    
-private:
-	ip::tcp::socket sock_;
-	enum { max_msg = 1024 };
-	int already_read_;
-	char buff_[max_msg];
-	bool started_;
-	std::string username_;
+class Client{
+    private:
+        ip::tcp::socket sock;
+        enum { max_msg = 1024 };
+    	int already_read_;
+    	char buff_[max_msg];
+    	bool started_;
+    	std::string username_;
+        bool clients_changed_;
+	    boost::posix_time::ptime last_ping;
+    public:
+        Client() : sock(service),clients_changed_(false) {}
+        
+        ip::tcp::socket& sock_r(){
+            return sock;
+        }
+        
+        std::string username() const { 
+            return username_; 
+        }
+        
+        void answer_to_client(){
+            try {
+		        read_request();
+		        process_request();
+	        } 
+	        catch ( boost::system::system_error&) 
+	        {
+		        stop();
+	        }
+	        if ( timed_out()) 
+		        stop(); 
+        }
+        
+        void read_request() {
+            if (sock.available())
+			    already_read_ += sock.read_some(buffer(buff_ + already_read_, max_msg - already_read_));
+        }
+        
+        void process_request() {
+            bool found_enter = std::find(buff_, buff_ + already_read_, '\n') < buff_ + already_read_;
+	        if ( !found_enter)
+		        return; 
+	       
+	        last_ping = boost::posix_time::microsec_clock::local_time();
+	        size_t pos = std::find(buff_, buff_ + already_read_, '\n') - buff_;
+	        std::string msg(buff_, pos);
+	        std::copy(buff_ + already_read_, buff_ + max_msg, buff_);
+	        already_read_ -= pos + 1;
+	        
+	        
+	        if ( msg.find("login ") == 0) on_login(msg);
+	        else if ( msg.find("ping") == 0) on_ping();
+	        else if ( msg.find("ask_clients") == 0) on_clients();
+	        else std::cerr << "invalid msg " << msg << std::endl;
+        }
+        
+        void on_login(const std::string & msg){
+            std::istringstream in(msg);
+	        in >> username_ >> username_;
+	        //std::cout<< username_ <<std::endl;
+	        write("login ok\n");
+	        {
+	        boost::recursive_mutex::scoped_lock lk(mx);
+	        for(unsigned i=0;i<clients.size()-1;i++)
+	            clients[i]->update_clients_changed();
+	        }
+        }
+        
+        void update_clients_changed(){
+            clients_changed_ = true;
+        }
+        
+        void on_ping(){
+            //std::cout<<clients_changed_<<std::endl;
+            write(clients_changed_ ? "ping client_list_changed\n" : "ping ok\n");
+	        clients_changed_ = false;
+        }
+        
+        void on_clients(){
+            std::string msg;
+	        { 
+		        boost::recursive_mutex::scoped_lock lk(mx);
+		        for(auto b = clients.begin(), e = clients.end() ;b != e; ++b)
+			        msg += (*b)->username() + " "; 
+	        }
+	        write("clients " + msg + "\n");
+        }
+        
+        void write(const std::string & msg) {
+            //cout<<msg<<endl;
+            sock.write_some(buffer(msg)); 
+        }
+        
+        void stop() {
+		    boost::system::error_code err;
+		    sock.close(err);
+	    }
+        
+        bool timed_out() const {
+    		boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
+    		long long ms = (now - last_ping).total_milliseconds();
+    		return ms > 5000 ;
+	    }
 };
 
 
-void run_client(const std::string & client_name){
-    ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8001);
-	talk_to_svr client(client_name);
-	try{
-		client.connect(ep);
-		client.loop();
-	}
-	catch(boost::system::system_error & err){
-		std::cout << "client terminated " << std::endl;
+void accept_thread(){
+    ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8001));
+    while(true){
+
+        std::shared_ptr<Client> cl = std::make_shared<Client>();
+        std::cout<<"wait client"<<std::endl;
+        acceptor.accept(cl->sock_r());
+        std::cout<<"client acepted"<<std::endl;
+        boost::recursive_mutex::scoped_lock lock(mx);
+        clients.push_back(cl);
+    }
+}
+
+void handle_clients_thread(){
+    while (true) {
+		boost::this_thread::sleep(boost::posix_time::millisec(1));
+		boost::recursive_mutex::scoped_lock lk(mx);
+		for(auto b = clients.begin(); b != clients.end(); ++b) 
+		    (*b)->answer_to_client();
+
+		clients.erase(std::remove_if(clients.begin(), clients.end(), 
+		boost::bind(&Client::timed_out,_1)), clients.end());
 	}
 }
 
+
 int main(){
-    run_client("dima");
-    //run_client("vasya");
-    //run_client("lenya");
-
-
+	boost::thread_group threads;
+	threads.create_thread(accept_thread);
+	threads.create_thread(handle_clients_thread);
+	threads.join_all();
 
     return 0;
 }
